ca7709835e6d2458d11f12c666e50d3d1bca55ad
ofer affias <ofer.affias@gmail.com>
2013-03-02 17:37:20 +0200

    better implementation which keeps user's cursor position

--- StickySearch.py
+++ StickySearch.py
@@ -6,33 +6,26 @@ class StickysearchCommand(sublime_plugin.TextCommand):
 		# keep sticky per window (each window has its own set)
 		view = self.view
 		key = "StickySearch"
-		view.window().run_command('find_all_under')
-		
+
 		if 'add' in op:
-			self.add(key, view)
+			regions = self.find_all_under_cursor(self.view) + view.get_regions(key)
+			self.mark(key, view, regions)	
 		if 'clear' in op:
-			self.clear(key, view)
+			view.erase_regions(key)
 		if 'set' in op:
-			self.set(key, view)			
-		
-		self.clear_selection(view)
-
-	def set(self, key, view):
-		self.mark(key, view, [])	
-
-	def add(self, key, view):
-		self.mark(key, view, view.get_regions(key))	
+			regions = self.find_all_under_cursor(self.view)
+			self.mark(key, view, regions)
 		
 	def mark(self, key, view, regions):
-		for s in view.sel():
-			regions.append(sublime.Region(s.begin(), s.end()))
 		# optional icon name, if given, will draw the named icons in the gutter next to each region. 
 		# The icon will be tinted using the color associated with the scope. 
-		# Valid icon names are dot, circle, bookmark 
-		view.add_regions(key, regions, "marker", "dot", sublime.PERSISTENT+sublime.DRAW_OUTLINED)
+		# Valid icon names are dot, circle, bookmark and cross
+		view.add_regions(key, regions, "marker", "dot", sublime.PERSISTENT | sublime.DRAW_OUTLINED)
 
-	def clear(self, key, view):
-		view.erase_regions(key)   
-
-	def clear_selection(self, view):
-		view.sel().clear()
+	def find_all_under_cursor(self, view):
+		# view.window().run_command('find_all_under')
+		the_cursor = view.sel()[0]
+		the_word_region = view.word(the_cursor)
+		the_word = view.substr(the_word_region)
+		all_word_regions = view.find_all("\\b" + the_word + "\\b")
+		return all_word_regions
